89ae4d2c704f375914b45e748b17f6c9f323af47
nosrick <nosrick@gmail.com>
2021-03-17 17:13:15 +0000

    Adds Rect2Int

---
+++ Assets/Scripts/JoyObject/Rect2Int.cs
@@ -0,0 +1,38 @@
+﻿namespace JoyLib.Code
+{
+    public struct Rect2Int
+    {
+        public int x;
+        public int y;
+
+        public int width;
+        public int height;
+
+        public int xMax => this.x + this.width;
+        public int yMax => this.y + this.height;
+
+        public int Area => this.width * height;
+
+        public Rect2Int(Vector2Int position, Vector2Int sizes)
+        {
+            this.x = position.x;
+            this.y = position.y;
+            this.width = sizes.x;
+            this.height = sizes.y;
+        }
+
+        public bool Contains(Vector2Int point)
+        {
+            if (point.x < this.x || point.x > this.x)
+            {
+                return false;
+            }
+            if (point.y < this.y || point.y > this.y)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
