2311d25a60d000e33d7fe7a938d806c85f01999e
nosrick <nosrick@gmail.com>
2021-03-18 12:40:35 +0000

    Biological sex processor now loads data!

--- Assets/Scripts/Entities/Sexes/EntityBioSexHandler.cs
+++ Assets/Scripts/Entities/Sexes/EntityBioSexHandler.cs
@@ -2,9 +2,13 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using Godot;
+using Godot.Collections;
 using JoyLib.Code.Entities.Sexes.Processors;
 using JoyLib.Code.Helpers;
 using JoyLib.Code.Scripting;
+using Directory = System.IO.Directory;
+using File = System.IO.File;
 
 namespace JoyLib.Code.Entities.Sexes
 {
@@ -32,61 +36,57 @@ namespace JoyLib.Code.Entities.Sexes
                 .ToDictionary(processor => processor.Name, processor => processor);
             
             string[] files =
-                Directory.GetFiles(Directory.GetCurrentDirectory() + GlobalConstants.DATA_FOLDER + "/Sexes",
-                    "*.json", SearchOption.AllDirectories);
+                Directory.GetFiles(
+                    Directory.GetCurrentDirectory() +
+                    GlobalConstants.ASSETS_FOLDER + 
+                    GlobalConstants.DATA_FOLDER + 
+                    "/Sexes",
+                    "*.json", 
+                    SearchOption.AllDirectories);
             foreach(string file in files)
             {
-                /*
-                using (StreamReader reader = new StreamReader(file))
+                JSONParseResult result = JSON.Parse(File.ReadAllText(file));
+
+                if (result.Error != Error.Ok)
                 {
-                    using (JsonTextReader jsonReader = new JsonTextReader(reader))
-                    {
-                        try
-                        {
-                            JObject jToken = JObject.Load(jsonReader);
-
-                            if (jToken.IsNullOrEmpty())
-                            {
-                                continue;
-                            }
-
-                            foreach (JToken child in jToken["Sexes"])
-                            {
-                                string name = (string) child["Name"];
-                                bool canBirth = (bool) (child["CanBirth"] ?? false);
-                                bool canFertilise = (bool) (child["CanFertilise"] ?? false);
-                                string processorString = (string) (child["Processor"] ?? "Neutral");
-                                IEnumerable<string> tags = child["Tags"] is null
-                                    ? new string[0]
-                                    : child["Tags"].Select(token => (string) token);
-
-                                IBioSexProcessor processor =
-                                    this.Processors.Values.FirstOrDefault(preferenceProcessor => 
-                                        preferenceProcessor.Name.Equals(processorString, StringComparison.OrdinalIgnoreCase)) ??
-                                    new NeutralProcessor();
-                                
-                                sexes.Add(
-                                    new BaseBioSex(
-                                        name,
-                                        canBirth,
-                                        canFertilise,
-                                        processor,
-                                        tags));
-                            }
-                        }
-                        catch (Exception e)
-                        {
-                            GlobalConstants.ActionLog.AddText("Could not load sexes in " + file);
-                            GlobalConstants.ActionLog.StackTrace(e);
-                        }
-                        finally
-                        {
-                            jsonReader.Close();
-                            reader.Close();
-                        }
-                    }
+                    this.ValueExtractor.PrintFileParsingError(result, file);
+                    continue;
                 }
-                */
+
+                if (!(result.Result is Dictionary dictionary))
+                {
+                    GlobalConstants.ActionLog.Log("Could not parse JSON in " + file + " into a Dictionary.", LogLevel.Warning);
+                    continue;
+                }
+
+                ICollection<Dictionary> sexesCollection =
+                    this.ValueExtractor.GetArrayValuesCollectionFromDictionary<Dictionary>(dictionary, "Sexes");
+
+                foreach (Dictionary innerDict in sexesCollection)
+                {
+                    string name = this.ValueExtractor.GetValueFromDictionary<string>(innerDict, "Name");
+                    bool canBirth = this.ValueExtractor.GetValueFromDictionary<bool>(innerDict, "CanBirth");
+                    bool canFertilise = this.ValueExtractor.GetValueFromDictionary<bool>(innerDict, "CanFertilise");
+                    string processorString = innerDict.Contains("Processor")
+                        ? this.ValueExtractor.GetValueFromDictionary<string>(innerDict, "Processor")
+                        : "Neutral";
+
+                    ICollection<string> tags =
+                        this.ValueExtractor.GetArrayValuesCollectionFromDictionary<string>(innerDict, "Tags");
+
+                    IBioSexProcessor processor =
+                        this.Processors.Values.FirstOrDefault(preferenceProcessor => 
+                            preferenceProcessor.Name.Equals(processorString, StringComparison.OrdinalIgnoreCase)) ??
+                        new NeutralProcessor();
+                                
+                    sexes.Add(
+                        new BaseBioSex(
+                            name,
+                            canBirth,
+                            canFertilise,
+                            processor,
+                            tags));
+                }
             }
 
             sexes.AddRange(ScriptingEngine.Instance.FetchAndInitialiseChildren<IBioSex>());
