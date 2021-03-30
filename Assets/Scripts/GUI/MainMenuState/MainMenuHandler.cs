2e5653bb3564add6ac81d9907ee363523aa552dc
nosrick <nosrick@gmail.com>
2021-03-30 11:29:25 +0100

    Just testing something.

--- Assets/Scripts/GUI/MainMenuState/MainMenuHandler.cs
+++ Assets/Scripts/GUI/MainMenuState/MainMenuHandler.cs
@@ -1,13 +1,35 @@
-﻿using Godot;
+﻿using System.Linq;
+using Godot;
+using JoyGodot.Assets.Scripts.GUI.Managed_Assets;
+using JoyLib.Code.Graphics;
 using JoyLib.Code.States;
 
 namespace JoyLib.Code.Unity.GUI.MainMenuState
 {
-    public class MainMenuHandler : Control
+    public class MainMenuHandler : GUIData
     {
+        public override void _Ready()
+        {
+            ManagedUIElement background = new ManagedUIElement
+            {
+                AnchorBottom = 1,
+                AnchorRight = 1
+            };
+
+            ISpriteState state = new SpriteState(
+                "default",
+                GlobalConstants.GameManager.ObjectIconHandler.GetSprites("Windows", "DefaultWindow").First());
+            
+            background.AddSpriteState(state);
+            
+            this.AddChild(background);
+            this.MoveChild(background, 0);
+        }
+
         public void NewGame()
         {
             GlobalConstants.GameManager.SetNextState(new CharacterCreationState());
+            GlobalConstants.GameManager.GUIManager.CloseAllGUIs();
         }
     }
 }
