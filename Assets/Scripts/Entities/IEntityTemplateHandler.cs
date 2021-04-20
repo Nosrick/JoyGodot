004554d76291d17596be7e765afd98e7b4604c88
nosrick <nosrick@gmail.com>
2021-04-20 16:37:14 +0100

    Minor optimisation of IEntityTemplateHandler

--- Assets/Scripts/Entities/IEntityTemplateHandler.cs
+++ Assets/Scripts/Entities/IEntityTemplateHandler.cs
@@ -1,11 +1,7 @@
-﻿using System.Collections.Generic;
-
-namespace JoyLib.Code.Entities
+﻿namespace JoyLib.Code.Entities
 {
     public interface IEntityTemplateHandler: IHandler<IEntityTemplate, string>
     {
-        IEntityTemplate Get(string type);
         IEntityTemplate GetRandom();
-        
     }
 }
