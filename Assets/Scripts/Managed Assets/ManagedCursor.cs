ab5230663f28c482f0231babd90b8d15b4530c5c
nosrick <nosrick@gmail.com>
2021-05-02 16:18:56 +0100

    Adding ManagedCursor.

---
+++ Assets/Scripts/Managed Assets/ManagedCursor.cs
@@ -0,0 +1,43 @@
+﻿using Godot;
+using JoyGodot.Assets.Scripts.GUI.Managed_Assets;
+using JoyLib.Code.Graphics;
+
+namespace JoyGodot.Assets.Scripts.Managed_Assets
+{
+    public class ManagedCursor : ManagedUIElement
+    {
+        protected ManagedUIElement DragObject { get; set; }
+
+        public ISpriteState DragSprite
+        {
+            get => this.DragObject.CurrentSpriteState;
+            set
+            {
+                this.DragObject.Clear();
+                if (value is null)
+                {
+                    return;
+                }
+                
+                this.DragObject.AddSpriteState(value);
+            }
+        }
+
+        public override void _Ready()
+        {
+            base._Ready();
+
+            this.DragObject = this.GetNode<ManagedUIElement>("Drag Object");
+        }
+
+        public override void _GuiInput(InputEvent @event)
+        {
+            base._GuiInput(@event);
+
+            if (@event is InputEventMouseMotion motion)
+            {
+                this.SetGlobalPosition(motion.GlobalPosition);
+            }
+        }
+    }
+}
