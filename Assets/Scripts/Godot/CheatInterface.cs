d368572136e7a494d2a9017da285781893e9b67d
nosrick <nosrick@gmail.com>
2021-05-17 08:58:11 +0100

    Adds Cheat Interface

---
+++ Assets/Scripts/Godot/CheatInterface.cs
@@ -0,0 +1,42 @@
+﻿using Godot;
+using JoyLib.Code.Unity.GUI;
+
+namespace JoyLib.Code.Godot
+{
+    public class CheatInterface : GUIData
+    {
+        public void FillNeeds()
+        {
+            var player = GlobalConstants.GameManager.Player;
+
+            if (player is null)
+            {
+                return;
+            }
+
+            foreach (var need in player.Needs.Values)
+            {
+                need.Fulfill(need.HappinessThreshold);
+            }
+
+            player.HappinessIsDirty = true;
+        }
+
+        public void EmptyNeeds()
+        {
+            var player = GlobalConstants.GameManager.Player;
+
+            if (player is null)
+            {
+                return;
+            }
+
+            foreach (var need in player.Needs.Values)
+            {
+                need.Decay(need.HappinessThreshold);
+            }
+
+            player.HappinessIsDirty = true;
+        }
+    }
+}
