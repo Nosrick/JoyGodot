1972a735a07d0875725332e0f405545c146a55ad
nosrick <nosrick@gmail.com>
2021-05-21 15:09:38 +0100

    Inventory screen now has a drag handle!

---
+++ Assets/Scripts/GUI/DragHandle.cs
@@ -0,0 +1,56 @@
+﻿using Godot;
+
+namespace JoyLib.Code.Unity.GUI
+{
+    public class DragHandle : Control
+    {
+        protected Control Parent { get; set; }
+        
+        protected bool Dragging { get; set; }
+        protected bool FirstMove { get; set; }
+
+        public override void _Ready()
+        {
+            base._Ready();
+
+            this.Parent = this.GetParent<Control>();
+        }
+
+        public override void _Input(InputEvent @event)
+        {
+            if (@event is InputEventMouseButton mouseButton)
+            {
+                if (mouseButton.ButtonIndex != (int) ButtonList.Left)
+                {
+                    return;
+                }
+                if (mouseButton.Pressed
+                && this.GetGlobalRect().HasPoint(mouseButton.GlobalPosition))
+                {
+                    this.Dragging = true;
+                    this.FirstMove = true;
+                }
+                else if (mouseButton.Pressed == false)
+                {
+                    this.Dragging = false;
+                }
+            }
+            else if (@event is InputEventMouseMotion mouseMotion)
+            {
+                if (!this.Dragging)
+                {
+                    return;
+                }
+                
+                if (this.FirstMove)
+                {
+                    this.FirstMove = false;
+                }
+                else
+                {
+                    this.Parent.RectGlobalPosition += mouseMotion.Relative;
+                }
+            }
+        }
+    }
+}
