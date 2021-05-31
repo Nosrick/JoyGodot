dd1d436d2255414698775a5481cbc624b57be442
nosrick <nosrick@gmail.com>
2021-05-31 12:39:15 +0100

    Minor tweak to how job experience discounts are fetched

--- Assets/Scripts/Entities/Jobs/JobType.cs
+++ Assets/Scripts/Entities/Jobs/JobType.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using Godot;
 using JoyLib.Code.Entities.Abilities;
 using JoyLib.Code.Helpers;
@@ -40,19 +41,27 @@ namespace JoyLib.Code.Entities.Jobs
 
         public int GetSkillDiscount(string skillName)
         {
-            if(this.m_SkillDiscounts.ContainsKey(skillName))
+            if (this.m_SkillDiscounts.Any(p =>
+                p.Key.Equals(skillName, StringComparison.OrdinalIgnoreCase)))
             {
-                return this.m_SkillDiscounts[skillName];
+                return this.m_SkillDiscounts.First(
+                        pair => pair.Key.Equals(skillName, StringComparison.OrdinalIgnoreCase))
+                    .Value;
             }
+
             return 0;
         }
 
         public int GetStatisticDiscount(string statisticName)
         {
-            if(this.m_StatisticDiscounts.ContainsKey(statisticName))
+            if (this.m_StatisticDiscounts.Any(p =>
+                p.Key.Equals(statisticName, StringComparison.OrdinalIgnoreCase)))
             {
-                return this.m_StatisticDiscounts[statisticName];
+                return this.m_StatisticDiscounts.First(
+                        pair => pair.Key.Equals(statisticName, StringComparison.OrdinalIgnoreCase))
+                    .Value;
             }
+
             return 0;
         }
 
