02c2893ec71827ab7a2b0fd6f6a9cbfdea6b9007
nosrick <nosrick@gmail.com>
2021-07-08 08:46:53 +0100

    Adding basic of recipes

---
+++ Assets/Scripts/Items/Crafting/CraftingRecipe.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using JoyGodot.Assets.Scripts.Collections;
+
+namespace JoyGodot.Assets.Scripts.Items.Crafting
+{
+    public class CraftingRecipe : IRecipe
+    {
+        public NonUniqueDictionary<string, int> RequiredMaterials { get; protected set; }
+        
+        public BaseItemType CraftingResult { get; protected set; }
+
+        public CraftingRecipe(
+            NonUniqueDictionary<string, int> requiredMaterials,
+            BaseItemType craftingResult)
+        {
+            this.RequiredMaterials = requiredMaterials;
+            this.CraftingResult = craftingResult;
+        }
+
+        public bool CanCraft(NonUniqueDictionary<string, int> materials)
+        {
+            bool result = true;
+            
+            foreach (Tuple<string, int> tuple in this.RequiredMaterials)
+            {
+                var recipeMaterials = this.RequiredMaterials.FetchValuesForKey(tuple.Item1);
+                List<int> resultsList = materials.FetchValuesForKey(tuple.Item1);
+                resultsList = resultsList
+                    .OrderByDescending(i => i)
+                    .ToList();
+                if (resultsList.Count < recipeMaterials.Count)
+                {
+                    result = false;
+                    break;
+                }
+
+                for (int i = 0; i < recipeMaterials.Count; i++)
+                {
+                    int resultsMin = resultsList.Min();
+                    int recipeMin = recipeMaterials.Min();
+                    
+                    if (resultsMin < recipeMin)
+                    {
+                        result = false;
+                        break;
+                    }
+
+                    resultsList.Remove(resultsMin);
+                }
+
+                if (!result)
+                {
+                    break;
+                }
+            }
+            
+            return result;
+        }
+    }
+}
---
+++ Assets/Scripts/Items/Crafting/IRecipe.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using JoyGodot.Assets.Scripts.Collections;
+
+namespace JoyGodot.Assets.Scripts.Items.Crafting
+{
+    public interface IRecipe
+    {
+        NonUniqueDictionary<string, int> RequiredMaterials { get; }
+
+        BaseItemType CraftingResult { get; }
+
+        bool CanCraft(NonUniqueDictionary<string, int> materials);
+    }
+}
