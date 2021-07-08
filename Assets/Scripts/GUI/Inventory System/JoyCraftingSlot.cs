309fd6300b4171e12373fa346ab89cd24070dedd
nosrick <nosrick@gmail.com>
2021-07-08 09:54:07 +0100

    Adding JoyCraftingSlot.

---
+++ Assets/Scripts/GUI/Inventory System/JoyCraftingSlot.cs
@@ -0,0 +1,27 @@
+﻿namespace JoyGodot.Assets.Scripts.GUI.Inventory_System
+{
+    public class JoyCraftingSlot : JoyConstrainedSlot
+    {
+        public int AmountRequired { get; set; }
+
+        public float AmountInSlot
+        {
+            get
+            {
+                if (this.m_Item is null)
+                {
+                    return 0;
+                }
+
+                if (this.m_Item.HasTag(this.Slot))
+                {
+                    return this.m_Item.ItemType.Size;
+                }
+
+                return 0;
+            }
+        }
+
+        public bool SufficientMaterial => this.AmountInSlot >= this.AmountRequired;
+    }
+}
