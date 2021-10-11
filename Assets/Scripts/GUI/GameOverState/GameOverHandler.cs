ef1bed0496ddd36025c7ffe5f14057544823a664
nosrick <nosrick@gmail.com>
2021-10-11 09:55:43 +0100

    Adding GameOver scene

---
+++ Assets/Scripts/GUI/GameOverState/GameOverHandler.cs
@@ -0,0 +1,22 @@
+﻿namespace JoyGodot.Assets.Scripts.GUI.GameOverState
+{
+    public class GameOverHandler : GUIData
+    {
+        public override void _Ready()
+        {
+            GlobalConstants.GameManager.GUIManager.SetupManagedComponents(this);
+        }
+
+        public void NewGame()
+        {
+            GlobalConstants.GameManager.GUIManager.CloseAllGUIs();
+            GlobalConstants.GameManager.SetNextState(new States.CharacterCreationState());
+        }
+
+        public void QuitToMenu()
+        {
+            GlobalConstants.GameManager.GUIManager.CloseAllGUIs();
+            GlobalConstants.GameManager.SetNextState(new States.MainMenuState());
+        }
+    }
+}
